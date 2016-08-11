dbec1c4cd70f33a2a9e81225a474349197542e32
robwannell <rob@meltemiweb.com>
2016-08-11 07:33:42 -0700

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:robwannell/prop58.git'
 
 set :deploy_to, '/home/deploy/prop58'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
