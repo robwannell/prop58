386cc71bc4983dc426b3be816f61db3ed304cac2
robwannell <rob@meltemiweb.com>
2016-08-15 09:28:40 -0700

    sp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.turbolinks
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
